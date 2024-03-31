fb695c84721fcb64f742e9f9c5284958169a6fe5
zainabibraheem <zaynarb96@gmail.com>
2024-03-31 14:00:54 +0100

    my portfolio

---
+++ src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <h2>ZAINAB</h2>
+        <div>
+            <ul>
+                <li> <a href=''>About me</a></li>
+                <li> <a href=''>Skills</a></li>
+                <li> <a href=''>Projects</a></li>
+                <li> <a href='' > Blog</a></li>
+                <li> <a href='' > Contact </a></li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
