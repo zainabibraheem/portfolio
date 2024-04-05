26cb6ca56994a150ed686e961f1d59fba704b458
zainabibraheem <zaynarb96@gmail.com>
2024-04-05 01:34:44 +0100

    fix

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -2,8 +2,12 @@ import React, { useState } from 'react';
 import { TfiEmail } from "react-icons/tfi";
 import { FaTwitter } from "react-icons/fa";
 import { MdEmail } from 'react-icons/md';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+
+    const form = useRef();
 
     const [formData, setFormData] = useState({
         username: '',
@@ -19,10 +23,28 @@ const Contact = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        setFormData({});
-        alert('Your message has been sent to our team, we will get back to you via your email');
-        console.log(formData);
-    }
+
+        emailjs
+            .sendForm('service_z5am25v', 'template_ig1s7e6', form.current, {
+                publicKey: 'PYkMRr6CtbRm9AmEb',
+            })
+            .then(
+                () => {
+                    alert('Message sent successfully');
+                },
+                (error) => {
+                    alert('Message not sent, pls try again later', error.text);
+                },
+            );
+
+        setFormData({
+            username: '',
+            email: '',
+            message: ''
+        });
+
+        event.target.reset();
+    };
 
     return (
         <div className='bg-black px-8  py-12'>
@@ -41,7 +63,7 @@ const Contact = () => {
 
                 <div className='text-white flex gap-5 flex-col justify-center items-center'>
                     <div className='border-[1px] flex flex-col items-center gap-1 bg-[#3B7141] rounded-md w-full lg:w-[18rem]  border-main text-center py-4'>
-                        <MdEmail size={25}/>
+                        <MdEmail size={25} />
                         <h2>Email</h2>
                         <h2> <a href='' >zaynarb96@gmail.com</a> </h2>
                         <h2 className='text-main font-bold'>Send me a message</h2>
@@ -56,10 +78,10 @@ const Contact = () => {
                 </div>
 
                 <div className=''>
-                    <form onSubmit={handleSubmit} className='flex flex-col gap-8'>
+                    <form ref={form} onSubmit={handleSubmit} className='flex flex-col gap-8'>
                         <input
                             placeholder='Name'
-                            className='w-full border bg-transparent border-main focus:outline-none focus:border-blue-500 rounded-md px-4 py-[7px]'
+                            className='w-full border bg-transparent border-main focus:outline-none focus:border-blue-500 text-complex rounded-md px-4 py-[7px]'
                             id='user-name'
                             name='username'
                             value={formData.username}
@@ -67,7 +89,7 @@ const Contact = () => {
                         />
                         <input
                             placeholder='Email'
-                            className='w-full bg-transparent border border-main focus:outline-none focus:border-blue-500 rounded-md px-4 py-[7px]'
+                            className='w-full bg-transparent border border-main focus:outline-none focus:border-blue-500 text-complex rounded-md px-4 py-[7px]'
                             id='email'
                             name='email'
                             value={formData.email}
@@ -75,7 +97,7 @@ const Contact = () => {
                         />
                         <textarea
                             placeholder='Message'
-                            className='w-full bg-transparent border border-main focus:outline-none focus:border-blue-500 rounded-md px-4 py-[7px]'
+                            className='w-full bg-transparent border border-main focus:outline-none focus:border-blue-500 text-complex rounded-md px-4 py-[7px]'
                             id='message'
                             rows='8'
                             name='message'
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
                 <div className='text-main'>---------------</div>
             </div>
             <h2 className='text-complex text-md'>Over time, As I've progressed in my journey of learning and mastering frontend tools and frameworks, I've built several projects to exhibit my skills.</h2>
-            <div className='bg-black gap-6 justify-center grid md:grid-cols-2 lg:grid-cols-3 py-10 px-[auto]'>
+            <div className='bg-black gap-6 grid justify-items-center md:grid-cols-2 lg:grid-cols-3 py-10 px-[auto]'>
                 {projectData.map((project) => {
                     const { id, image, title, about, link } = project
                     return (
