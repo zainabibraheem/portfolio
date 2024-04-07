f2a7fdf01a495caf12a3b2930102c5241bd3a72d
zainabibraheem <zaynarb96@gmail.com>
2024-04-07 14:13:44 +0100

    fix: contact

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -16,10 +16,10 @@ const Footer = () => {
             </div>
 
             <div className='flex gap-4 mb-3'>
-              <a href='' > <FaGithub size={20}/> </a>
-              <a href='' > <SlSocialLinkedin size={20}/> </a>
-              <a href='' > <SiHashnode size={20}/> </a>
-              <a href=''> <FaXTwitter size={20} /> </a>
+              <a href='https://github.com/zainabibraheem' > <FaGithub size={20}/> </a>
+              <a href='https://www.linkedin.com/in/zainabibrahim-/' > <SlSocialLinkedin size={20}/> </a>
+              <a href='https://zainabibrahim.hashnode.dev/' > <SiHashnode size={20}/> </a>
+              <a href='https://twitter.com/zainabibraheem_'> <FaXTwitter size={20} /> </a>
             </div>
         </div>
     )
